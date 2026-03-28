88730b6ab0bab3ed23159de25f86c4e7de6e15ad
Kashaf Zia <kashafzia100@gmail.com>
2026-03-28 14:18:18 +0500

    task 2 added

---
+++ Q2/FolderStructure/src/App.jsx
@@ -0,0 +1,18 @@
+
+import reactLogo from './images/react.svg'
+import header from './components/Header'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      {header}
+      <img src={reactLogo} alt="" />
+    </>
+  )
+}
+
+export default App
